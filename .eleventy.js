7885b275689d86ff9f8ca8199908b5f52718253f
reiniscirpons <rc234@st-andrews.ac.uk>
2021-07-15 21:42:51 +0300

    Fix custom domain

--- .eleventy.js
+++ .eleventy.js
@@ -60,6 +60,8 @@ module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  // Copy `CNAME` file so custom domains work
+  eleventyConfig.addPassthroughCopy("CNAME");
 
   // Customize Markdown library and settings:
   let markdownLibrary = markdownIt({
